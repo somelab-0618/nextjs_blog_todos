6e51e6a1b08931ad7d0b1874bbf579967a0f979c
RyosukeSomeya <rx.0.0618@gmail.com>
2022-06-01 01:25:54 +0900

    Login, Sign up機能追加

--- components/Auth.js
+++ components/Auth.js
@@ -1,58 +1,125 @@
 import { LockClosedIcon } from '@heroicons/react/solid';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import Cookie from 'universal-cookie'; // JWT tokenを格納する
+import Image from 'next/image';
+
+const cookie = new Cookie();
 
 export default function Auth() {
+  // 関数で遷移させる
+  const router = useRouter();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogin, setIsLogin] = useState(true);
+
+  // APIにアクセスし、JWTトークンを取得する
+  const login = async () => {
+    try {
+      await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create/`, {
+        method: 'POST',
+        body: JSON.stringify({ username: username, password: password }),
+        headers: {
+          'Content-type': 'application/json',
+        },
+      })
+        .then((res) => {
+          if (res.status === 400) {
+            throw 'authentication failded';
+          } else if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          const options = { path: '/' };
+          cookie.set('access_token', data.access, options);
+        });
+      router.push('/main-page');
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const authUser = async (e) => {
+    e.preventDefault();
+    if (isLogin) {
+      login();
+    } else {
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register/`, {
+          method: 'POST',
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            'Content-type': 'application/json',
+          },
+        }).then((res) => {
+          if (res.status === 400) {
+            throw 'authentication failded';
+          }
+        });
+        login();
+      } catch (err) {
+        alert(err);
+      }
+    }
+  };
+
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-50">
-        <body class="h-full">
-        ```
-      */}
-      <div className='min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
-        <div className='max-w-md w-full space-y-8'>
+      <div className='w-2/4 min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+        <div className='max-w-md w-full space-y-8 text-center	'>
           <div>
-            <img
+            <Image
               className='mx-auto h-12 w-auto'
               src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
               alt='Workflow'
+              width={64}
+              height={64}
             />
+
             <h2 className='mt-6 text-center text-3xl font-extrabold text-white'>
-              Sign in to your account
+              {isLogin ? 'Login' : 'Sign up'}
             </h2>
           </div>
-          <form className='mt-8 space-y-6' action='#' method='POST'>
+          <form className='mt-8 space-y-6' onSubmit={authUser}>
             <input type='hidden' name='remember' defaultValue='true' />
             <div className='rounded-md shadow-sm -space-y-px'>
               <div>
                 <input
-                  id='email-address'
-                  name='email'
-                  type='email'
-                  autoComplete='email'
+                  name='username'
+                  type='text'
+                  autoComplete='username'
                   required
                   className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-                  placeholder='Email address'
+                  placeholder='username'
+                  value={username}
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
                 />
               </div>
               <div>
                 <input
-                  id='password'
                   name='password'
                   type='password'
                   autoComplete='current-password'
                   required
                   className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
                   placeholder='Password'
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
                 />
               </div>
             </div>
 
             <div className='flex items-center justify-center'>
               <div className='text-sm'>
-                <span className='cursor-pointer font-medium text-white'>
+                <span
+                  onClick={() => setIsLogin(!isLogin)}
+                  className='cursor-pointer font-medium text-white'
+                >
                   change mode?
                 </span>
               </div>
@@ -69,7 +136,7 @@ export default function Auth() {
                     aria-hidden='true'
                   />
                 </span>
-                Sign in
+                {isLogin ? 'Login with JWT' : 'Create new user'}
               </button>
             </div>
           </form>
